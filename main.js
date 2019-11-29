bc111ecf52e901a94cffa608c33e03f3b28c077c
Olivia Davis <olivia.davis593@gmail.com>
2019-11-29 16:31:17 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+$(function() {
+    console.log('App loaded!');
+    returnDogImage();
+});
+
+function userInput() {
+   let breed = $('#dog-breed').val();
+   return breed;
+}
+
+function returnDogImage() {
+    $('#dog-input-form').submit(e => {
+        e.preventDefault();
+        retrieveImage();
+    });
+}
+
+function retrieveImage() {
+    fetch('https://dog.ceo/api/breed/" + getUserInput() + "/images/random')
+        .then(response => response.json())
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => alert('I cant seem to find that. Try something else!'));
+}
+
+function disaplayResults(responseJson)  {
+    console.log(responseJson);
+    if(responseJson.status !== 'success') {
+        alert('I cant seem to find that. Try something else!');
+    }
+    else if (responseJson.status === 'success') {
+        $('.results').replaceWith (
+            `<img src="${responseJson.message}" class="results">` 
+        );
+        $('.results').removeClass('hidden');
+    }
+}
